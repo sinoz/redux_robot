efacdc279e9cc7da6b3f315e5910e588cdd4dfe4
Elmer Bulthuis <elmerbulthuis@gmail.com>
2019-10-04 10:21:15 +0200

    changes test so that the line length is max 80 and t.end is not needed

--- src/middleware/RobotMiddleware.spec.ts
+++ src/middleware/RobotMiddleware.spec.ts
@@ -1,12 +1,18 @@
 import * as test from "blue-tape";
 import { applyMiddleware, createStore } from "redux";
-import { advanceAlphabeticLetter, turnRobotOn } from "../actions/RobotAction";
+import { advanceAlphabeticLetter } from "../actions/RobotAction";
 import { robotStatusReducer } from "../reducer/RobotReducer";
 import { createActivityFilter } from "./RobotMiddleware";
 
-test("The middleware should throw an error when attempting to make alphabetic advancements when the robot is still turned off", (t) => {
-    const store = createStore(robotStatusReducer, applyMiddleware(createActivityFilter()));
+test(
+    `The middleware should throw an error when attempting to make ` +
+    `alphabetic advancements when the robot is still turned off`,
+    async (t) => {
+        const store = createStore(
+            robotStatusReducer,
+            applyMiddleware(createActivityFilter()),
+        );
 
-    t.throws(() => store.dispatch(advanceAlphabeticLetter));
-    t.end();
-});
+        t.throws(() => store.dispatch(advanceAlphabeticLetter));
+    },
+);
